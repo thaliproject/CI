e79eb1c561256529294bb86393b3f5b2c1793dc6
Ilya Laryionau <larryonoff@gmail.com>
2016-11-04 12:08:26 +0300

    fixing some linter issues

--- hook/git_request.js
+++ hook/git_request.js
@@ -11,16 +11,17 @@ exports.OnRequest = function (req, res) {
   }
 
   if (json.pull_request) { // pull request
-    if (json.action != "opened" && json.action != "reopened"
-        && json.action != "synchronize") {
-      console.log("Skipping PR > ", json.action, json.number, json.pull_request.title);
+    if (json.action != 'opened' &&
+        json.action != 'reopened' &&
+        json.action != 'synchronize') {
+      console.log('Skipping PR > ', json.action, json.number, json.pull_request.title);
       return;
     }
 
     try {
       require('fs').writeFileSync('last_request.json', JSON.stringify(json, null, 2));
-    } catch(e_) {
-      console.error("Couldn't write the JSON", e_);
+    } catch (e_) {
+      console.error('Couldn\'t write the JSON', e_);
     }
 
     var prNumber = json.number;
@@ -28,7 +29,7 @@ exports.OnRequest = function (req, res) {
     var prId = pr.id; // this needs to be unique!! see if we tested this PR before
     var user = pr.user.login; // username
     var title = pr.title; // title of commit / PR
-    var branch, repo_name = "";
+    var branch, repo_name = '';
     var commits = 9999999;
     var target_branch = pr.base.ref;
 
@@ -39,17 +40,18 @@ exports.OnRequest = function (req, res) {
       }
     }
 
-    if (pr && pr.state === "open") { // PR state
+    if (pr && pr.state === 'open') { // PR state
       var head = pr.head;
       branch = head.ref;
       repo_name = head.repo.full_name;
     }
 
-    if (!repo_name || repo_name.indexOf("thaliproject/") != 0 &&
-      repo_name.indexOf("obastemur/") != 0) {
-      if (pr && pr.state && pr.state != "open") {
+    if (!repo_name || repo_name.indexOf('thaliproject/') != 0 &&
+      repo_name.indexOf('obastemur/') != 0) {
+      if (pr && pr.state && pr.state != 'open') {
+        // nothing to do
       } else {
-        logme("BAD REQUEST from repo", repo_name, "red");
+        logme('BAD REQUEST from repo', repo_name, 'red');
       }
     } else {
       if (db.hasJob(prNumber, commits)) return;
@@ -57,17 +59,17 @@ exports.OnRequest = function (req, res) {
       if (jobs_done[prNumber + commits]) return;
 
       jobs_done[prNumber + commits] = 1;
-      logme("PR >", prId, commits, prNumber, user, title, repo_name, branch, "yellow");
+      logme('PR >', prId, commits, prNumber, user, title, repo_name, branch, 'yellow');
       git.newTest(prId, prNumber, user, title, repo_name, branch, commits, target_branch);
     }
-  } else if (json.hook && (typeof json.hook_id !== "undefined")) { // new web hook
+  } else if (json.hook && (typeof json.hook_id !== 'undefined')) { // new web hook
     var hook_id = json.hook_id;
     var repo_name = json.repository.full_name;
     var branch = json.repository.default_branch;
     var user = json.sender.login;
 
-    if (repo_name.indexOf("thaliproject/") != 0 &&
-      repo_name.indexOf("obastemur/") != 0) {
+    if (repo_name.indexOf('thaliproject/') != 0 &&
+      repo_name.indexOf('obastemur/') != 0) {
       logme("BAD REQUEST from repo", repo_name, "red");
     } else {
       // check if we have the repo on DB
@@ -92,6 +94,6 @@ exports.OnRequest = function (req, res) {
                              the hook registered into db*/ 99999);
     }
   } else {
-    logme("Unkown Request Received", "red");
+    logme('Unkown Request Received', 'red');
   }
 };
