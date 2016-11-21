a55d7a66977b3c9be614a6874c695752559fed42
Ilya Laryionau <ilaryionau@ilyas-imac-2.hiqo-solutions.com>
2016-11-21 13:32:57 +0300

    migrate `runServerRemote.sh` into `pipefail`

--- tasker/runServerRemote__.sh
+++ tasker/runServerRemote__.sh
@@ -1,31 +1,27 @@
 #!/usr/bin/env bash
+
 ### START - JXcore Test Server --------
 ### Testing environment prepares separate packages for each node.
 ### Package builder calls this script with each node's IP address
 ### Make sure multiple calls to this script file compiles the application file
 
-NORMAL_COLOR=''
-RED_COLOR=''
-GREEN_COLOR='\033[0;32m'
-GRAY_COLOR='\033[0;37m'
+log_error() {
+  local filename=$(basename "$0")
+  local linenumber=${1}
+  local code="${2:-1}"
 
-LOG() {
-  COLOR="$1"
-  TEXT="$2"
-  echo -e "${COLOR}$TEXT ${NORMAL_COLOR}"
+  NORMAL_COLOR='\033[0m'
+  RED_COLOR='\033[0;31m'
+
+  echo -e "${RED_COLOR}error: command '${BASH_COMMAND}' failed with code ${code}, file '${filename}' on line ${linenumber}${NORMAL_COLOR}"
 }
 
+set -euo pipefail
 
-ERROR_ABORT() {
-  if [[ $? != 0 ]]
-  then
-    LOG $RED_COLOR "$1\n"
-    LOG $RED_COLOR "Run IS script aborted\n"
-    exit -1
-  fi
-}
+trap 'log_error $LINENO' ERR
+
 ### END - JXcore Test Server   --------
 
-cd ~/Test/{{SERVER_LOCATION}};ERROR_ABORT
+cd ~/Test/{{SERVER_LOCATION}}
 sudo pkill jx
-sudo jx ______.js {{TARGET}};ERROR_ABORT
+sudo jx ______.js {{TARGET}}
